278f1d9e3ad62562af6c0bcffefe23a61da1bb01
Alec Graves <me@alecgraves.net>
2025-02-12 01:58:07 -0600

    add simple framebuffer code

---
+++ test/glfw_framebuffer_simple.c
@@ -0,0 +1,241 @@
+// clang-15 -Oz -o simple_test glfw_framebuffer_simple.c -L../build -I../vendor/glfw/include -lglfw3 -lm -lGLESv2 -lX11 -I. -Wl,--gc-sections,icf=all,--strip-all -flto
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <GLES2/gl2.h>
+#include <GLFW/glfw3.h>
+
+int WIDTH = 640, HEIGHT = 480;
+int MIN_W = 480;
+int MIN_H = 360;
+int RENDER_W = 640;
+int RENDER_H = 480;
+int PREV_RENDER_W = 640;
+int PREV_RENDER_H = 480;
+int MAX_RENDER = 2048;
+
+GLuint program;
+GLuint texture;
+GLuint vbo;
+unsigned char *pixels = NULL;
+
+const char* vertexShaderSource =
+	"attribute vec2 position;\n"
+	"attribute vec2 tex_coord;\n"
+	"varying vec2 v_tex_coord;\n"
+	"void main() {\n"
+	"  gl_Position = vec4(position, 0.0, 1.0);\n"
+	"  v_tex_coord = tex_coord;\n"
+	"}\n";
+
+const char* fragmentShaderSource =
+	"precision mediump float;\n"
+	"varying vec2 v_tex_coord;\n"
+	"uniform sampler2D u_texture;\n"
+	"void main() {\n"
+	"  gl_FragColor = texture2D(u_texture, v_tex_coord);\n"
+	"}\n";
+
+static inline void resize_up_to(int width, int height, int* outwidth, int* outheight, int max) {
+	if(width > max || height > max){
+		if (width > height) {
+			*outwidth = max;
+			*outheight = (float)height/width*max ;
+			return;
+		} else {
+			*outheight = max;
+			*outwidth = (float)width/height*max;
+			return;
+		}
+	}
+	*outwidth = width;
+	*outheight = height;
+}
+
+void mouse_callback(GLFWwindow* window, int button, int action, int mods) {
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+		// Handle left mouse button press
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+		printf("Left mouse button pressed at (%f, %f)\n", xpos, ypos);
+	}
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_BACKSPACE) {
+		// TODO exponential speedup to max delete (erase current fungi)
+		if (action == GLFW_PRESS) {
+			printf("PACKSPACE PRESSED\n");
+		}
+		if (action == GLFW_RELEASE) {
+			printf("PACKSPACE RELEASED\n");
+		}
+	}
+	// todo undo
+	if (key == GLFW_KEY_Z && (mods & GLFW_MOD_CONTROL) && !(mods & GLFW_MOD_SHIFT) && (action == GLFW_PRESS)) {
+		printf("UNDO PRESSED\n");
+	}
+}
+
+// This callback is called whenever the framebuffer is resized.
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	resize_up_to(width, height, &RENDER_W, &RENDER_H, MAX_RENDER);
+	WIDTH = width;
+	HEIGHT = height;
+	printf("Window resized to %d x %d, rendering  %d x %d \n", width, height, RENDER_W, RENDER_H);
+}
+
+void init() {
+	if (!glfwInit()) {
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		exit(EXIT_FAILURE);
+	}
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL ES Noise Demo", NULL, NULL);
+	if (!window) {
+		fprintf(stderr, "Failed to create window\n");
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetWindowSizeLimits(window, MIN_W, MIN_H, GLFW_DONT_CARE, GLFW_DONT_CARE);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	// glfwSwapInterval(1); // glfw pause for screen update
+	glfwSwapInterval(0); // no pause for screen update
+
+	// gui io
+	glfwSetMouseButtonCallback(window, mouse_callback);
+	glfwSetKeyCallback(window, key_callback);
+
+	// Allocate pixel buffer for the texture dimensions.
+	pixels = (unsigned char*)malloc(MAX_RENDER * MAX_RENDER * 3);
+	// seed rng
+	srand(time(0));
+
+
+	// Compile shaders.
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertexShaderSource, NULL);
+	glCompileShader(vs);
+	// (Consider adding error checking here.)
+
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fs);
+	// (Consider adding error checking here.)
+
+	program = glCreateProgram();
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	// (Check linking errors if desired.)
+	glUseProgram(program);
+
+	// Create a VBO with interleaved position and texture coordinates.
+	// We use a triangle strip for the full-screen quad.
+	float vertices[] = {
+		// Position    // Tex Coords
+		-1.0f, -1.0f,  0.0f, 0.0f, // bottom left
+		 3.0f, -1.0f,  2.0f, 0.0f, // bottom right + extra
+		-1.0f,  3.0f,  0.0f, 2.0f  // top left + extra
+	};
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	GLint posAttrib = glGetAttribLocation(program, "position");
+	glEnableVertexAttribArray(posAttrib);
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+
+	GLint texAttrib = glGetAttribLocation(program, "tex_coord");
+	glEnableVertexAttribArray(texAttrib);
+	glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
+	// Create and configure the texture.
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); // GL_LINEAR
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+
+	// do not initialize storage
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, RENDER_W, RENDER_H, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glActiveTexture(GL_TEXTURE0);
+
+	GLint texUniform = glGetUniformLocation(program, "u_texture");
+	glUniform1i(texUniform, 0);
+}
+
+void draw(unsigned char* pixels_rgb) {
+	/// 1. Resize if needed.
+	// if resize, make new texture
+	if (RENDER_H != PREV_RENDER_H || RENDER_W != PREV_RENDER_W) {
+		// Reinitialize the texture storage to the new dimensions.
+		glBindTexture(GL_TEXTURE_2D, texture);
+		// Passing NULL as data to allocate storage without initializing it.
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, RENDER_W, RENDER_H, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+		// // Reinitialize the viewport.
+		// glViewport(0, 0, WIDTH, HEIGHT);
+		PREV_RENDER_H = RENDER_H;
+		PREV_RENDER_W = RENDER_W;
+		glViewport(0, 0, WIDTH, HEIGHT);
+	}
+
+	/// 2. Update Pixels
+	// glBindTexture(GL_TEXTURE_2D, texture);
+	// Update the texture with the new data.
+	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, RENDER_W, RENDER_H, GL_RGB, GL_UNSIGNED_BYTE, pixels_rgb);
+
+	/// 3. Display
+	// glClear(GL_COLOR_BUFFER_BIT);
+	// glActiveTexture(GL_TEXTURE0);
+	// glBindTexture(GL_TEXTURE_2D, texture);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glfwSwapBuffers(glfwGetCurrentContext());
+}
+
+
+int main() {
+	init();
+	GLFWwindow* window = glfwGetCurrentContext();
+
+	int i = 0;
+	while (!glfwWindowShouldClose(window)) {
+		// Generate new random noise in the pixel buffer.
+		for (int i = 0; i < RENDER_W * RENDER_H; i+=4) {
+			int r = rand();
+			pixels[i * 3 + 0] = r & 255;
+			pixels[i * 3 + 1] = (r * r) & 255;
+			pixels[i * 3 + 2] = (r + 17) & 255;
+			r = r >> 7;
+			pixels[(i+1) * 3 + 0] = r & 255;
+			pixels[(i+1) * 3 + 1] = (r * r) & 255;
+			pixels[(i+1) * 3 + 2] = (r + 17) & 255;
+			r = r >> 7;
+			pixels[(i+2) * 3 + 0] = r & 255;
+			pixels[(i+2) * 3 + 1] = (r * r) & 255;
+			pixels[(i+2) * 3 + 2] = (r + 17) & 255;
+			r = r >> 7;
+			pixels[(i+3) * 3 + 0] = r & 255;
+			pixels[(i+3) * 3 + 1] = (r * r) & 255;
+			pixels[(i+3) * 3 + 2] = (r + 17) & 255;
+		}
+		draw(pixels);
+		glfwPollEvents();
+		i += 1;
+		printf("i=%d\n", i);  // 30 fps small
+	}
+
+	glDeleteProgram(program);
+	glDeleteBuffers(1, &vbo);
+	glDeleteTextures(1, &texture);
+	glfwTerminate();
+	free(pixels);
+	return 0;
+}
